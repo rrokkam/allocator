5229737639fd6562600b8b5b65e7f6d00f89cafd
rrokkam <rrokkam@cs.stonybrook.edu>
2018-02-25 16:20:15 -0500

    updated to do list

--- src/main.c
+++ src/main.c
@@ -10,31 +10,30 @@
  * TODO:
  *
  * - Replace the old criterion tests
- * - Rewrite debug functions to print out state of lists
  * - Clean up some spots with messy formatting / too much macro usage.
  * - Coalescing properly, in both directions
  * - Document the new stuff
- *
- * Extra stuff, if I have time
  * - Redo the headers -- this is big!
- * - magic numbers to check for corruption
+ * - extra: magic numbers to check for corruption
  * 
  * Already done
- * - Set up tests with criterion (includes Makefile work). DONE
- * - Add glibc-style linear/logarithmic bin sizes. DONE
- * - add calloc. DONE
- * - Set actual errno instead of ye_errno. DONE
+ * - Set up tests with criterion (includes Makefile work)
+ * - Add glibc-style linear/logarithmic bin sizes
+ * - add calloc
+ * - Set actual errno instead of ye_errno
+ * - Rewrite debug functions to print out state of lists
  *
  */
 int main(int argc, char const *argv[]) {
-    setup();
-    void *x = ye_malloc(sizeof(double) * 8);
-    // ye_varprint(x);
-    // ye_snapshot();
-    void *z = ye_realloc(x, sizeof(int));
-    // ye_varprint(y);
-    // ye_snapshot();
-    (void) z;
-    teardown();
-    return 2;
+    exit(EXIT_SUCCESS); // placeholder for compilation
+    // setup();
+    // void *x = ye_malloc(sizeof(double) * 8);
+    // // ye_varprint(x);
+    // // ye_snapshot();
+    // void *z = ye_realloc(x, sizeof(int));
+    // // ye_varprint(y);
+    // // ye_snapshot();
+    // (void) z;
+    // teardown();
+    // return 2;
 }
